830a2008dde1e48edf1d3082ccba0f2a33a89047
Gonzalo Herrera <hgonzalo94@gmail.com>
2017-01-15 18:44:32 -0300

    setup initial / web

---
+++ HomeCinema.Web/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using HomeCinema.Web.Infrastructure.MessageHandlers;
+using System.Web.Http;
+
+namespace HomeCinema.Web {
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            config.MessageHandlers.Add(new HomeCinemaAuthHandler());
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ HomeCinema.Web/Infrastructure/Extensions/RequestMessageExtensions.cs
@@ -0,0 +1,17 @@
+﻿using HomeCinema.Services.Abstract;
+using System.Net.Http;
+using System.Web.Http.Dependencies;
+
+namespace HomeCinema.Web.Infrastructure.Extensions {
+    public static class RequestMessageExtensions {
+        internal static IMembershipService GetMembershipService(this HttpRequestMessage
+        request) {
+            return request.GetService<IMembershipService>();
+        }
+        private static TService GetService<TService>(this HttpRequestMessage request) {
+            IDependencyScope dependencyScope = request.GetDependencyScope();
+            TService service = (TService)dependencyScope.GetService(typeof(TService));
+            return service;
+        }
+    }
+}
